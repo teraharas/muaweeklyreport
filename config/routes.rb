425bdc4de3241c97165518d23463fd1b47c1e320
teraharas <triple.zero.option@gmail.com>
2015-09-13 13:09:18 +0000

    Add SFA Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages_controller#index'
 
+  get '/auth/:provider/callback' => 'sessions#callback'
+  get '/signout' => 'sessions#destroy', :as => :signout
+
 end
